2a3d233e5635f339c149fb27b1f0d38dc7e45a50
ayetinvakti <hercayi9@gmail.com>
2026-08-06 12:45:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,49 @@
+const CACHE_NAME = 'ayetim-vakti-v4';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon.png'
+];
 
+// Kurulum aşaması
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assetsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Eski önbellekleri temizleme
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Önce sunucudan taze veriyi çek, internet yoksa önbelleğe git
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((networkResponse) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
